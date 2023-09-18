0b5b504e553c32a86235f3d304f43cea8cbb7cac
Jean-Marc <33706536+jmwages@users.noreply.github.com>
2023-09-17 23:44:54 -0400

    Started making obstacles
    Started adding obstacles, class will probably actually be obstacle sprite.
    basic framework started

---
+++ csc121-creative-project/src/Obstacle.java
@@ -0,0 +1,39 @@
+class Obstacle {
+	
+	Posn pos;
+	Bounds bounds;
+	int width;
+	int height;
+	
+	boolean offScreen = false;
+	
+	int track;
+	
+	float gameSpd;
+	
+	Obstacle(int width, int height, int track, float gameSpd) {
+		switch (track) {
+		case 1:
+			this.pos = new Posn(300, 200);
+			break;
+		case 2:
+			this.pos = new Posn(600, 200);
+			break;
+		case 3:
+			this.pos = new Posn(900, 200);
+			break;
+		}
+		
+		this.width = width;
+		this.height = height;
+		this.bounds = new Bounds(pos, width, height);
+		
+		this.track = track;
+		this.gameSpd = gameSpd;
+	}
+	
+
+	
+	
+
+}
